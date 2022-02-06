472fef8e15af28636a58abd6ddb8ef82b73be3b1
KeplerBoyce <keplerboy@gmail.com>
2022-02-06 01:37:38 -0800

    communication with opponent's client works

--- app.js
+++ app.js
@@ -12,35 +12,39 @@ answerList.forEach(word => {
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/lobby.html', function(req, res) {
+    res.sendFile(__dirname + '/lobby.html');
+});
 app.get('/game.html', function(req, res) {
     res.sendFile(__dirname + '/game.html');
 });
 
 rooms = [];
 io.on('connection', function(socket) {
-    console.log('a user connected');
+    console.log('a user connected, id=' + socket.id);
     socket.on('attemptCreate', function(data) {
         if (data.roomcode in rooms) {
             socket.emit('roomCodeTaken', data.roomcode);
             console.log('room code ' + data.roomcode + ' is taken');
         } else {
-            rooms[data.roomcode] = [data.username];
-            socket.emit('joinRoom', data.roomcode);
+            rooms[data.roomcode] = {usernames: [data.username], userIds: [socket.id]};
+            socket.emit('joinRoom', {username: data.username, roomcode: data.roomcode});
             console.log(data.username + ' created room ' + data.roomcode);
         }
         console.log(rooms);
     });
     socket.on('attemptJoin', function(data) {
         if (data.roomcode in rooms) {
-            if (rooms[data.roomcode].length === 2) {
+            if (rooms[data.roomcode].userIds.length === 2) {
                 socket.emit('roomFull', data.roomcode);
-                console.log('Cannot join; room ' + data.roomcode + ' is full');
-            } else if (rooms[data.roomcode].indexOf(data.username) > -1) {
+                console.log('can\'t join; room ' + data.roomcode + ' is full');
+            } else if (rooms[data.roomcode].usernames.indexOf(data.username) > -1) {
                 socket.emit('userAlreadyInRoom', {username: data.username, roomcode: data.roomcode});
-                console.log('Already a user ' + data.username + ' in room ' + data.roomcode);
+                console.log('already a user ' + data.username + ' in room ' + data.roomcode);
             } else {
-                rooms[data.roomcode].push(data.username);
-                socket.emit('joinRoom', data.roomcode);
+                rooms[data.roomcode].usernames.push(data.username);
+                rooms[data.roomcode].userIds.push(socket.id);
+                socket.emit('joinRoom', {username: data.username, roomcode: data.roomcode});
                 console.log(data.username + ' joined room ' + data.roomcode);
             }
         } else {
@@ -51,6 +55,23 @@ io.on('connection', function(socket) {
     });
     socket.on('requestAnswersAndGuesses', function() {
         socket.emit('answersAndGuesses', {answers: answerList, guesses: guessList});
+        console.log('sent answer and guess lists to client');
+    });
+    socket.on('sendColorsToServer', function(data) {
+        var userIds = rooms[data.roomcode].userIds;
+        var opponentId;
+        if (userIds[0] === socket.id) opponentId = userIds[1];
+        else opponentId = userIds[0];
+        socket.broadcast.emit('sendColorsToClient', {idValue: opponentId, colors: data.colors, row: data.row});
+        console.log('sent colors to session id ' + opponentId);
+    });
+    socket.on("requestOpponentId", function(data) {
+        var userIds = rooms[data.roomcode].userIds;
+        var opponentId;
+        if (userIds[0] === socket.id) opponentId = userIds[1];
+        else opponentId = userIds[0];
+        socket.emit('opponentId', {idValue: opponentId});
+        console.log('sent opponent id to ' + socket.id);
     });
     socket.on('disconnect', function() {
         console.log("a user disconnected");
