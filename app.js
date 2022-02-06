92ebc5611ae01c54af8f635a7f4ba60b6d9aa329
KeplerBoyce <keplerboy@gmail.com>
2022-02-05 18:15:49 -0800

    main menu ui

--- app.js
+++ app.js
@@ -2,19 +2,47 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-//Whenever someone connects this gets executed
-io.on('connection', function(socket){
+rooms = [];
+io.on('connection', function(socket) {
     console.log('A user connected');
-    
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-       console.log('A user disconnected');
-    });
- });
- http.listen(3000, function(){
+    socket.on('attemptCreate', function(data) {
+        var roomIndex = -1;
+        for (var i = 0; i < rooms.length; i++) {
+            if (rooms[i].roomcode == data.roomcode) roomIndex = i;
+        }
+        if (roomIndex > -1) {
+            socket.emit('roomCodeTaken', data.roomcode);
+            console.log('room code ' + data.roomcode + ' is taken');
+        } else {
+            rooms.push({roomcode: data.roomcode, users: [data.username]});
+            socket.emit('joinRoom', data.roomcode);
+            console.log(data.username + ' created room ' + data.roomcode);
+        }
+    })
+    socket.on('attemptJoin', function(data) {
+        var roomIndex = -1;
+        for (var i = 0; i < rooms.length; i++) {
+            if (rooms[i].roomcode == data.roomcode) roomIndex = i;
+        }
+        if (roomIndex > -1) {
+            if (rooms[roomIndex].users.indexOf(data.username) > -1) {
+                socket.emit('userAlreadyInRoom', {username: data.username, roomcode: data.roomcode});
+                console.log('Already a user ' + data.username + ' in room ' + data.roomcode);
+            } else {
+                rooms[roomIndex].users.push(data.username);
+                console.log(data.username + ' joined room ' + data.roomcode);
+            }
+        } else {
+            socket.emit('invalidCode', data.roomcode);
+            console.log('room ' + data.roomcode + ' does not exist');
+        }
+    })
+});
+
+http.listen(3000, function() {
     console.log('listening on *:3000');
- });
+});
